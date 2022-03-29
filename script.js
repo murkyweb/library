3d7c3ac63d63cd16bc80d8ec34489af071b23748
murkyweb <murkyweb@protonmail.com>
2022-03-29 22:13:28 +0300

    Added function for displaying objects from the array, as cards, on the page

--- script.js
+++ script.js
@@ -1,5 +1,4 @@
-let myLibrary = [];
-
+let myLibrary = [{author: 'Alexandre Dumas', title: 'The Count of Monte Cristo', pages: 1276}]
 function Book(author, title, pages) {
     this.author = author;
     this.title = title;
@@ -12,4 +11,29 @@ function addBookToLibrary() {
     const pages = prompt('Number of pages: ');
     const book = new Book(author, title, pages);
     myLibrary.push(book);
+}
+
+function displayLibrary() {
+    for(let i = 0; i < myLibrary.length; i++) {
+        if(!myLibrary[i].show) {
+            const container = document.querySelector('.container');
+
+            let card = document.createElement('div');
+            let cardAuthor = document.createElement('p');
+            let cardTitle = document.createElement('p');
+            let cardPages = document.createElement('p');
+
+            cardAuthor.innerText = myLibrary[i].author;
+            cardTitle.innerText = myLibrary[i].title;
+            cardPages.innerText = myLibrary[i].pages;
+
+            card.classList.add('card');
+            card.appendChild(cardAuthor);
+            card.appendChild(cardTitle);
+            card.appendChild(cardPages);
+            
+            container.appendChild(card);
+            myLibrary[i].show = true;
+        }
+    }
 }
