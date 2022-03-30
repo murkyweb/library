4b7402fa194ec3f3cf6263f0726c3c7affa5ea92
murkyweb <murkyweb@protonmail.com>
2022-03-30 22:01:36 +0300

    Added button to toggle read status of book

--- script.js
+++ script.js
@@ -3,17 +3,19 @@ let myLibrary = [];
 const submitButton = document.querySelector('.submit');
 const addBookButton = document.querySelector('.add-book');
 
-function Book(author, title, pages) {
+function Book(author, title, pages, read) {
     this.author = author;
     this.title = title;
     this.pages = pages;
+    this.read = read;
 }
 
 function addBookToLibrary() {
     let author = document.querySelector('#author').value;
     let title = document.querySelector('#title').value;
     let pages = +document.querySelector('#pages').value;
-    let book = new Book(author, title, pages);
+    let read = document.querySelector('#read').checked;
+    let book = new Book(author, title, pages, read);
     myLibrary.push(book);
 }
 
@@ -26,27 +28,37 @@ function displayLibrary() {
             let cardAuthor = document.createElement('p');
             let cardTitle = document.createElement('p');
             let cardPages = document.createElement('p');
+            let readButton = document.createElement('button');
             let deleteButton = document.createElement('button');
 
             cardAuthor.innerText = myLibrary[i].author;
             cardTitle.innerText = myLibrary[i].title;
             cardPages.innerText = myLibrary[i].pages;
+            readButton.innerText = myLibrary[i].read ? 'Read' : 'Not read';
             deleteButton.innerText = 'X';
-            deleteButton.classList.add('delete');
 
             card.classList.add('card');
+            deleteButton.classList.add('delete');
+            readButton.classList.add('.read-button');
+
             card.setAttribute('data-key', i);
 
             card.appendChild(cardAuthor);
             card.appendChild(cardTitle);
             card.appendChild(cardPages);
+            card.appendChild(readButton);
             card.appendChild(deleteButton);
             
             container.appendChild(card);
             myLibrary[i].show = true;
             deleteButton.addEventListener('click', () => {
-                let element = (document.querySelector(`.card[data-key='${i}']`));
+                let element = document.querySelector(`.card[data-key='${i}']`);
                 container.removeChild(element);
+            });
+
+            readButton.addEventListener('click', () => {
+                myLibrary[i].read = (myLibrary[i].read) ? false : true;
+                readButton.innerText = myLibrary[i].read ? 'Read' : 'Not read'; 
             });
         }
     }
@@ -56,6 +68,7 @@ function emptyForm() {
     document.querySelector('#author').value = '';
     document.querySelector('#title').value = '';
     document.querySelector('#pages').value = '';
+    document.querySelector('#read').checked = false;
 }
 
 function toggleForm() {
@@ -71,4 +84,3 @@ submitButton.addEventListener('click', () => {
     toggleForm();
     displayLibrary();
 });
-
